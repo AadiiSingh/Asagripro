94329bd67066bc006af633a2d07617faadcac04f
Aditya Bhadoria <adityabhadoria019@gmail.com>
2024-03-15 00:52:05 +0530

    Init commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors')
+
+const { port } = require("./config");
+
+
+app.use("/", express.static(path.join(__dirname, 'dist','website')));
+	app.get('/*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'dist', 'website', 'index.html'))
+  })
+
+  app.listen(port, () => {
+    console.log(`Server is connected at port....${port}`);
+  });
