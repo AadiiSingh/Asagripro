0f74505e22b6d5854f9f942c55ed588446fd122d
Aditya Bhadoria <adityabhadoria019@gmail.com>
2024-03-16 15:09:13 +0530

    title change, address change, phone change

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const cors = require('cors')
 const { port } = require("./config");
 
 
-app.use("/", express.static(path.join(__dirname, 'dist')));
+app.use("/", express.static(path.join(__dirname, 'docs')));
 	app.get('/*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+		res.sendFile(path.join(__dirname, 'docs', 'index.html'))
   })
 
   app.listen(port, () => {
